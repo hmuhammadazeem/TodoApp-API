4edd6d348cdb1bf06820c500815dd7ee4af7c1ea
Muhammad Azeem <azeem.rana85@gmail.com>
2018-08-21 01:20:42 +0500

    Defining the routes.

--- Step1A/Step1A.py
+++ Step1A/Step1A.py
@@ -9,27 +9,30 @@ mongo = PyMongo(app, uri="mongodb://127.0.0.1:27017/Todo-API")
 @app.route("/tasks", methods=['GET'])
 def get_tasks_list():
     data = mongo.db.tasks.find()
-    data_list = [{'title': task["title"], 'description': task["description"], 'done': task["done"]} for task in data]
+    data_list = [{'_id': int(task["_id"]), 'title': task["title"],
+                  'description': task["description"], 'done': task["done"]} for task in data]
     return jsonify({'Tasks': data_list})
 
 
-@app.route('/tasks/<id>', methods=['GET'])
-def get_task(id):
-    task = mongo.db.tasks.find_one_or_404({'_id': bson.ObjectId(oid=str(id))})
-    return jsonify({'title': task["title"], 'description': task["description"], 'done': task["done"]})
+@app.route('/task/<int:ID>', methods=['GET'])
+def get_task(ID):
+    task = mongo.db.tasks.find_one_or_404({'_id': ID})
+    return jsonify({'_id': int(task['_id']),
+                    'title': task["title"], 'description': task["description"], 'done': task["done"]})
 
 
-@app.route('/tasks', methods=['POST'])
+@app.route('/task/add', methods=['POST'])
 def add_task():
     mongo.db.tasks.insert(
-        {'_id': get_task_id(), 'title': request.form['title'], 'description': request.form['description'], 'done': request.form['done']})
+        {'_id': get_task_id(), 'title': request.form['title'],
+         'description': request.form['description'], 'done': request.form['done']})
     return 'Success!'
 
 
-@app.route('/task/<id>', methods=['PUT'])
-def update_task(id):
+@app.route('/task/update', methods=['PUT'])
+def update_task():
     tasks = mongo.db.tasks
-    task = tasks.find_one({'_id': bson.ObjectId(oid=str(id))})
+    task = tasks.find_one({'_id': request.form['task_id']})
     task["title"] = request.form['title']
     task["description"] = request.form['description']
     task["done"] = request.form['done']
@@ -37,18 +40,16 @@ def update_task(id):
     return get_tasks_list()
 
 
-@app.route('/tasks/<id>', methods=['DELETE'])
-def delete_task(id):
-    tasks = mongo.db.tasks
-    task = tasks.find_one({'_id': bson.ObjectId(oid=str(id))})
-    task.remove()
+@app.route('/task/delete/<int:ID>', methods=['DELETE'])
+def delete_task(ID):
+    mongo.db.tasks.delete_one({'_id': ID})
     return 'Task deleted successfully!'
 
 
 @app.route('/')
 def index():
     return """
-    <form action="/tasks" method="POST">
+    <form action="/task/add" method="POST">
         <input type="text" name="title" placeholder="Title"><br>
         <input type="text" name="description" placeholder="Description"><br>
         <input type="text" name="done" placeholder="Status"><br>
@@ -64,7 +65,6 @@ def get_task_id():
     id['sequence_value'] += 1
     value.save(id)
     return x
-
 
 
 if __name__ == '__main__':
